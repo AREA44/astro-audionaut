3f7341d5b93c4d160747d7417b3a4b8b66b4a826
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-09-08 01:19:26 +0700

    fix: wrong URL and add packageManager

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: 'Astro Audionaut',
   description: 'An audio player built with Astro.js',
-  url: 'https://astro-audionaunt.vercel.app',
-  ogImage: 'https://astro-audionaunt.vercel.app/og-image.png',
+  url: 'https://astro-audionaut.vercel.app',
+  ogImage: 'https://astro-audionaut.vercel.app/og-image.png',
   pageSize: 2,
 }
