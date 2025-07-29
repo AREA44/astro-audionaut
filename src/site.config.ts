c4449d935ff894a9230161933304e2a1ba8a32ee
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-29 16:06:38 +0700

    v25.7.30

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   title: "Astro Audionaut",
-  description: "An audio player built with Astro.js",
+  description: "An audio player built with Astro.",
   url: "https://astro-audionaut.vercel.app",
   ogImage: "https://astro-audionaut.vercel.app/og-image.png",
   pageSize: 5,
