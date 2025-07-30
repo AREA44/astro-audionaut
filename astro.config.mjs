8a05853499b3e072a3e6141ee67e18e50fa3b0e9
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-30 16:41:05 +0700

    enable deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+const site = process.env.VERCEL
+  ? process.env.VERCEL_ENV === "production"
+    ? "https://astro-audionaut.vercel.app"
+    : `https://${process.env.VERCEL_URL}`
+  : (process.env.SITE ?? "http://localhost:4321");
+const base = process.env.BASE || "/";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-audionaut.vercel.app",
+  site,
+  base,
   vite: {
     plugins: [tailwindcss()],
   },
